a8844625768d2da928a750e097cae0e155ab9cde
sbetoule <sylvain.betoule@gmail.com>
2026-04-01 10:58:14 +0200

    App.py

--- App.py
+++ App.py
@@ -1,20 +1,8 @@
 import streamlit as st
-import streamlit.components.v1 as components
+from streamlit_analytics2 import track
 
-def add_analytics():
-    ga_js = """
-    <script>
-        var script = window.parent.document.createElement('script');
-        script.async = true;
-        script.src = 'https://www.googletagmanager.com/gtag/js?id=G-G7EE1V3XFL';
-        window.parent.document.head.appendChild(script);
-
-        var config = window.parent.document.createElement('script');
-        config.innerHTML = "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-G7EE1V3XFL');";
-        window.parent.document.head.appendChild(config);
-    </script>
-    """
-    components.html(ga_js, height=0)
-
-# Appelez la fonction au début
-add_analytics()
+# On entoure tout le code par la fonction track()
+with track():
+    st.title("Annales Lab Chimie")
+    # ... le reste de votre code ...
+    st.write("Bienvenue sur l'application.")
