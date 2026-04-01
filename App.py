4b01d28e40a3ec20ae64cb3c6ced0b3716713cf6
sbetoule <sylvain.betoule@gmail.com>
2026-04-01 10:56:00 +0200

    App.py

--- App.py
+++ App.py
@@ -16,19 +16,6 @@ st.set_page_config(
         """
     })
 
-# 2. Injection du code Google Analytics
-ga_code = """
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-G7EE1V3XFL"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-G7EE1V3XFL');
-</script>
-"""
-
-# Utilisation d'un composant vide pour injecter le JS dans le DOM
-st.components.v1.html(ga_code, height=0)
 # --- STYLE CSS (LOGO, CRÉDITS, ANIMATION MOBILE) ---
 st.markdown("""
     <link rel="preconnect" href="https://fonts.googleapis.com">
